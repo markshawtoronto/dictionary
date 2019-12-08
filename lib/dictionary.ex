14564d8bbb34f7f4370bd4dcc82aa513beaa39fe
markshawtoronto <mark.shaw@mail.utoronto.ca>
2019-12-08 14:22:27 -0500

    Setup random_word selection.

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -3,4 +3,13 @@ defmodule Dictionary do
   def hello do
     "Hello world!!"
   end
+
+  def random_word do
+    Enum.random word_list
+  end
+
+  def word_list do
+    contents = File.read!("./assets/words.txt")
+    String.split contents
+  end
 end
--- test/dictionary_test.exs
+++ test/dictionary_test.exs
@@ -3,6 +3,17 @@ defmodule DictionaryTest do
   doctest Dictionary
 
   test "greets the world" do
-    assert Dictionary.hello() == "Hello world!!!"
+    assert Dictionary.hello() == "Hello world!!"
+  end
+
+  test "returns some words" do
+    list = Dictionary.word_list
+    assert length(list) == 8881
+  end
+
+  test "returns a random word in the word_list" do
+    list = Dictionary.word_list
+    random_word = Dictionary.random_word
+    assert Enum.member? list, random_word
   end
 end
